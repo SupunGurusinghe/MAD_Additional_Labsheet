d03028579acb2a3367427b51cad165ad8d9bde88
SupunGurusinghe <supunsameeran@gmail.com>
2021-08-22 15:08:17 +0530

    Setup notification

--- app/src/main/java/com/example/additionallabsheet/MainActivity.java
+++ app/src/main/java/com/example/additionallabsheet/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.additionallabsheet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.media.MediaPlayer;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.Settings;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
